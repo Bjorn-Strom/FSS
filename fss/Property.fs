be30eb0363675b1c74045323816a59e6a8856744
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2020-07-22 16:31:51 +0200

    Add all properties

--- fss/Property.fs
+++ fss/Property.fs
@@ -4,45 +4,266 @@ open Utilities.Types
 
 module Property =
     type Property =
+        | AlignContent
+        | AlignItems
+        | AlignSelf
+        | All
+        | Animation                     // Done
+        | AnimationDelay                // Done
+        | AnimationDirection            // Done
+        | AnimationDuration             // Done
+        | AnimationFillMode             // Done
+        | AnimationIterationCount       // Done
+        | AnimationName                 // Done
+        | AnimationPlayState            // Done
+        | AnimationTimingFunction       // Done
+        | BackfaceVisibility
+        | BackgroundAttachment
+        | BackgroundBlendMode
+        | BackgroundClip
+        | BackgroundColor               // Done
+        | BackgroundImage
+        | BackgroundOrigin
+        | BackgroundPosition
+        | BackgroundRepeat
+        | BackgroundSize
+        | Background
+        | Bleed
+        | BorderBottomColor             // Done
+        | BorderBottomLeftRadius        // Done
+        | BorderBottomRightRadius       // Done
+        | BorderBottomStyle
+        | BorderBottomWidth             // Done
+        | BorderBottom
+        | BorderCollapse
+        | BorderColor                   // Done
+        | BorderImage
+        | BorderImageOutset
+        | BorderImageRepeat
+        | BorderImageSource
+        | BorderImageSlice
+        | BorderImageWidth
+        | BorderLeftColor               // Done
+        | BorderLeftStyle
+        | BorderLeftWidth               // Done
+        | BorderLeft
+        | BorderRadius                  // Done
+        | BorderRightColor              // Done
+        | BorderRightStyle
+        | BorderRightWidth              // Done
+        | BorderRight
+        | BorderSpacing
+        | BorderStyle                    // Done
+        | BorderTopColor                 // Done
+        | BorderTopLeftRadius            // Done
+        | BorderTopRightRadius           // Done
+        | BorderTopStyle
+        | BorderTopWidth                 // Done
+        | BorderTop
+        | BorderWidth                    // Done
+        | Border                         // Done
+        | Bottom
+        | BoxDecorationBreak
+        | BoxShadow
+        | BoxSizing
+        | BreakAfter
+        | BreakBefore
+        | BreakInside
+        | CaptionSide
+        | CaretColor
+        | Clear
+        | Clip
+        | Color                             // Done
+        | Columns
+        | ColumnCount
+        | ColumnFill
+        | ColumnGap
+        | ColumnRule
+        | ColumnRuleColor
+        | ColumnRuleStyle
+        | ColumnRuleWidth
+        | ColumnSpan
+        | ColumnWidth
+        | Content
+        | CounterIncrement
+        | CounterReset
+        | CueAfter
+        | CueBefore
+        | Cue
+        | Cursor
+        | Direction
+        | Display
+        | Elevation
+        | EmptyCells
+        | Filter
+        | Flex
+        | FlexBasis
+        | FlexDirection
+        | FontFeatureSettings
+        | FlexFlow
+        | FlexGrow
+        | FlexShrink
+        | FlexWrap
+        | Float
+        | FontFamily
+        | FontKerning
+        | FontLanguageOverride
+        | FontSizeAdjust
+        | FontSize                   // Done
+        | FontStretch
+        | FontStyle
+        | FontSynthesis
+        | FontVariant
+        | FontVariantAlternates
+        | FontVariantCaps
+        | FontVariantEastAsian
+        | FontVariantLigatures
+        | FontVariantNumeric
+        | FontVariantPosition
+        | FontWeight
+        | Font
+        | GridArea
+        | GridAutoColumns
+        | GridAutoFlow
+        | GridAutoRows
+        | GridColumnEnd
+        | GridColumnGap
+        | GridColumnStart
+        | GridColumn
+        | GridGap
+        | GridRowEnd
+        | GridRowGap
+        | GridRowStart
+        | GridRow
+        | GridTemplateAreas
+        | GridTemplateColumns
+        | GridTemplateRows
+        | GridTemplate
+        | Grid
+        | HangingPunctuation
+        | Height
+        | Hyphens
+        | Isolation
+        | JustifyContent
+        | JustifyItems
+        | JustifySelf
         | Label
+        | Left
+        | LetterSpacing
+        | LineBreak
+        | LineHeight
+        | ListStyleImage
+        | ListStylePosition
+        | ListStyleType
+        | ListStyle
+        | MarginBottom
+        | MarginLeft
+        | MarginRight
+        | MarginTop
+        | Margin
+        | MarkerOffset
+        | Marks
+        | MaxHeight
+        | MaxWidth
+        | MinHeight
+        | MinWidth
+        | MixBlendMode
+        | NavUp
+        | NavDown
+        | NavLeft
+        | NavRight
+        | Opacity
+        | Order
+        | Orphans
+        | OutlineColor
+        | OutlineOffset
+        | OutlineStyle
+        | OutlineWidth
+        | Outline
+        | Overflow
+        | OverflowWrap
+        | OverflowX
+        | OverflowY
+        | PaddingBottom
+        | PaddingLeft
+        | PaddingRight
+        | PaddingTop
+        | Padding
+        | PageBreakAfter
+        | PageBreakBefore
+        | PageBreakInside
+        | Page
+        | PauseAfter
+        | PauseBefore
+        | Pause
+        | Perspective
+        | PerspectiveOrigin
+        | PitchRange
+        | Pitch
+        | PlaceContent
+        | PlaceItems
+        | PlaceSelf
+        | PlayDuring
+        | Position
+        | Quotes
+        | Resize
+        | RestAfter
+        | RestBefore
+        | Rest
+        | Richness
+        | Right
+        | Size
+        | SpeakHeader
+        | SpeakNumeral
+        | SpeakPunctuation
+        | Speak
+        | SpeechRate
+        | Stress
+        | TabSize
+        | TableLayout
+        | TextAlign
+        | TextAlignlast
+        | TextDecoration
+        | TextDecorationColor
+        | TextDecorationLine
+        | TextDecorationSkip
+        | TextDecorationStyle
+        | TextIndent
+        | TextOverflow
+        | TextShadow
+        | TextTransform
+        | TextUnderlinePosition
+        | Top
+        | Transform                      // Done
+        | TransformOrigin
+        | TransformStyle
+        | Transition                     // Done
+        | TransitionDelay
+        | TransitionDuration
+        | TransitionProperty
+        | TransitionTimingFunction
+        | UnicodeBidi
+        | VerticalAlign
+        | Visibility
+        | VoiceBalance
+        | VoiceDuration
+        | VoiceFamily
+        | VoicePitch
+        | VoiceRange
+        | VoiceRate
+        | VoiceStress
+        | VoiceVolume
+        | Volume
+        | WhiteSpace
+        | Widows
+        | Width
+        | WillChange
+        | WordBreak
+        | WordSpacing
+        | WordWrap
+        | ZIndex
 
         | Hover
-
-        | BackgroundColor
-        | FontSize
-        | Color
-
-        | Border
-        | BorderStyle
-        | BorderWidth
-        | BorderTopWidth
-        | BorderRightWidth
-        | BorderBottomWidth
-        | BorderLeftWidth
-        | BorderRadius
-        | BorderTopLeftRadius
-        | BorderTopRightRadius
-        | BorderBottomRightRadius
-        | BorderBottomLeftRadius
-        | BorderColor
-        | BorderTopColor
-        | BorderRightColor
-        | BorderBottomColor
-        | BorderLeftColor
-
-        | Animation
-        | AnimationName
-        | AnimationDuration
-        | AnimationTimingFunction
-        | AnimationDelay
-        | AnimationIterationCount
-        | AnimationDirection
-        | AnimationFillMode
-        | AnimationPlayState
-
-        | Transform
-
-        | Transition
 
         interface ICSSProperty
 
