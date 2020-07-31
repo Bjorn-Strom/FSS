28781c1827bbaa604a1f8f33de4688afaf70c3e4
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2020-07-31 20:41:26 +0200

    refactor tests

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -57,123 +57,38 @@ let getComputedCssById (id : string) : obj  = jsNative
 [<Emit("$0[$1];")>]
 let getValue (object: obj) (key: string) : string = jsNative
 
+let test (testName: string) (style: string) (propertyToTest: string list) (result: string) =
+    testCase' testName <| fun _ ->
+        RTL.render(
+            fragment []
+                [
+                    div [ Id "id"; ClassName style] []
+                ]) |> ignore
+
+        let computed = getComputedCssById("id")
+        Expect.equal (getValue computed propertyToTest) result testName
+
 let CssTests =
     testList "Css tests" [
-        testCase' "Style color with named color" <| fun _ ->
-            RTL.render(
-                fragment []
-                    [
-                        div 
-                            [ Id "color"; ClassName (fss [ Color aliceblue]) ]
-                            []
-
-                        div 
-                            [ Id "colorPink"; ClassName (fss [ BackgroundColor deeppink]) ]
-                            []
-                    ]
-            ) |> ignore
-            
-            let color = getComputedCssById("color")
-            let colorAlpha = getComputedCssById("colorPink")
-            Expect.equal (getValue color "color") "rgb(240, 248, 255)" "named color"
-            Expect.equal (getValue colorAlpha "background-color") "rgb(255, 20, 147)" "named background color"
-
-        testCase' "Style color with RGB" <| fun _ ->
-            RTL.render(
-                fragment []
-                    [
-                        div 
-                            [ Id "color"; ClassName (fss [ Color (rgb 255 0 0)]) ]
-                            []
-
-                        div 
-                            [ Id "colorAlpha"; ClassName (fss [ Color (rgba 255 255 255 0.5)]) ]
-                            []
-                    ]
-            ) |> ignore
-            
-            let color = getComputedCssById("color")
-            let colorAlpha = getComputedCssById("colorAlpha")
-            Expect.equal (getValue color "color") "rgb(255, 0, 0)" "color rgb style applied"
-            Expect.equal (getValue colorAlpha "color") "rgba(255, 255, 255, 0.5)" "color rgba style applied"
-
-        testCase' "Style color with HEX" <| fun _ ->
-            RTL.render(
-                fragment []
-                    [
-                        div 
-                            [ Id "color"; ClassName (fss [ Color (hex "ff0000")]) ]
-                            []
-
-                        div 
-                            [ Id "colorAlpha"; ClassName (fss [ Color (hex "ff000080")]) ]
-                            []
-
-                        div 
-                            [ Id "colorHash"; ClassName (fss [ Color (hex "#00FF00")]) ]
-                            []
-                    ]
-            ) |> ignore
-            
-            let color = getComputedCssById("color")
-            let colorAlpha = getComputedCssById("colorAlpha")
-            let colorHash = getComputedCssById("colorHash")
-            Expect.equal (getValue color "color") "rgb(255, 0, 0)" "color hex style applied"
-            Expect.equal (getValue colorAlpha "color") "rgba(255, 0, 0, 0.5)" "color hex style applied with alpha"
-            Expect.equal (getValue colorHash "color") "rgb(0, 255, 0)" "color hex style applied with hash"
-
-        testCase' "Style color with hsl" <| fun _ ->
-            RTL.render(
-                fragment []
-                    [
-                        div 
-                            [ Id "color"; ClassName (fss [ Color (hsl 120 1.0 0.5)]) ]
-                            []
-
-                        div 
-                            [ Id "colorAlpha"; ClassName (fss [ Color (hsla 120 1.0 0.5 0.5)]) ]
-                            []
-                    ]
-            ) |> ignore
-            
-            let color = getComputedCssById("color")
-            let colorAlpha = getComputedCssById("colorAlpha")
-            Expect.equal (getValue color "color") "rgb(0, 255, 0)" "color hsl style applied"
-            Expect.equal (getValue colorAlpha "color") "rgba(0, 255, 0, 0.5)" "color hsla style applied"
-
-        testCase' "Font-size" <| fun _ ->
-            RTL.render(
-                fragment []
-                    [
-                        div 
-                            [ Id "small"; ClassName (fss [ FontSize Small ]) ]
-                            []
-
-                        div 
-                            [ Id "large"; ClassName (fss [ FontSize Large ]) ]
-                            []
-
-
-                        div 
-                            [ Id "px"; ClassName (fss [ FontSize (px 100) ]) ]
-                            []
-
-                        div 
-                            [ Id "pct"; ClassName (fss [ FontSize (pct 200) ]) ]
-                            []
-                    ]
-            ) |> ignore
-            
-            let small = getComputedCssById("small")
-            let large = getComputedCssById("large")
-            let px = getComputedCssById("px")
-            let pct = getComputedCssById("pct")
-            Expect.equal (getValue small "font-size") "13px" "font size set to small"
-            Expect.equal (getValue large "font-size") "18px" "font size set to large"
-            Expect.equal (getValue px "font-size") "100px" "font size set with pixels"
-            Expect.equal (getValue pct "font-size") "32px" "font size set with percent"
-
         
+        test "Set named background color" (fss [ BackgroundColor red]) "background-color" "rgb(255, 0, 0)"
+        test "Set named color" (fss [ Color aliceblue]) "color" "rgb(240, 248, 255)"
+        test "Set color with rgb" (fss [ Color (rgb 255 0 0)]) "color" "rgb(255, 0, 0)"
+        test "Set color with rgba" (fss [ Color (rgba 255 0 0  0.5)]) "color" "rgba(255, 0, 0, 0.5)"
+        test "Set color with hex" (fss [ Color (hex "ff0000")]) "color" "rgb(255, 0, 0)"
+        test "Set color with hex with hash" (fss [ Color (hex "#ff0000")]) "color" "rgb(255, 0, 0)"
+        test "Set color with hex alpha" (fss [ Color (hex "ff000080")]) "color" "rgba(255, 0, 0, 0.5)"
+        test "Set color with HSL" (fss [ Color (hsl 120 1.0 0.5)]) "color"  "rgb(0, 255, 0)"
+        test "Set color with HSLA" (fss [ Color (hsla 120 1.0 0.5 0.5)]) "color" "rgba(0, 255, 0, 0.5)"
+
+        test "Set font to small" (fss [ FontSize XSmall ]) "font-size" "10px"
+        test "Set font to large" (fss [ FontSize Large ]) "font-size" "18px"
+        test "Set font to pixels" (fss [ FontSize (px 100) ]) "font-size" "100px"
+        test "Set font to percent" (fss [ FontSize (pct 200) ]) "font-size" "32px"
+
+        test "Set border radius" (fss [ BorderRadius (px 10)]) "border-top-left-radius" "10px"
+
+(*
         testCase' "Borders" <| fun _ ->
             RTL.render(
                 fragment []
@@ -606,6 +521,7 @@ let CssTests =
             Expect.equal (getValue eleven "background-clip") "border-box" "Set background clip"
 
             Expect.equal (getValue twelve "background-attachment") "scroll" "Set background attachment"
+            *)
     ]
 
 Mocha.runTests CssTests |> ignore
