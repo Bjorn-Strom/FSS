59eb3f5a517dab37218e5277b2567f572aebb88d
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-02-18 08:27:16 +0100

    Add marker pseudo element

--- fss/PseudoElement.fs
+++ fss/PseudoElement.fs
@@ -3,7 +3,6 @@
 open Fable.Core.JsInterop
 open Fss
 
-
 [<AutoOpen>]
 module PseudoElement =
     let private pseudoValue value attributeList = value ==> (attributeList |> fss) |> CSSProperty
@@ -13,9 +12,11 @@ module PseudoElement =
         static member FirstLetterElement (attributeList: CSSProperty list) = pseudoValue "::first-letter" attributeList
         static member FirstLineElement (attributeList: CSSProperty list) = pseudoValue "::first-line" attributeList
         static member SelectionElement (attributeList: CSSProperty list) = pseudoValue "::selection" attributeList
+        static member MarkerElement (attributeList: CSSProperty list) = pseudoValue "::marker" attributeList
 
     let After       (attributeList: CSSProperty list) = PseudoElement.AfterElement attributeList
     let Before      (attributeList: CSSProperty list) = PseudoElement.BeforeElement attributeList
     let FirstLetter (attributeList: CSSProperty list) = PseudoElement.FirstLetterElement attributeList
     let FirstLine   (attributeList: CSSProperty list) = PseudoElement.FirstLineElement attributeList
     let Selection   (attributeList: CSSProperty list) = PseudoElement.SelectionElement attributeList
+    let Marker      (attributeList: CSSProperty list) = PseudoElement.MarkerElement attributeList
