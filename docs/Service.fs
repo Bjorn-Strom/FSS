1f7e4d7c67d8ad757ad7b712cfbe2903ac89ce4f
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-03-01 17:19:30 +0100

    Start replacing inline docs with markdown

--- docs/Service.fs
+++
@@ -1,9 +0,0 @@
-namespace Docs
-
-module Service =
-    open Thoth.Fetch
-
-    let inline getMarkdown (markdown: string) =
-        promise {
-            return! Fetch.get($"https://raw.githubusercontent.com/Bjorn-Strom/FSS/master/documentation/{markdown}.md")
-        }
