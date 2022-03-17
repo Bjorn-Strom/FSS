3b178b39a0093f91cde34c033175a25c51cbf55e
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-03-17 22:06:07 +0100

    Add more tests for ordering

--- tests/Ordering.fs
+++ tests/Ordering.fs
@@ -6,11 +6,18 @@ open Fss
 open Fss.Types
 
 module OrderingTests =
+    let createOrderTest name rules =
+        createFssWithClassname name rules
+        |> snd
+        |> List.map (fun (x, y) ->
+            $"{x.ToString()} {y.ToString()}")
+        |> String.concat ""
+            
     let tests =
        testList "Ordering"
            [
-                let normalSelectorMedia =
-                    (createFssWithClassname "orderingOne" [
+                let orderingOne =
+                    (createOrderTest "orderingOne" [
                             Display.flex
                             FlexDirection.column
                             
@@ -34,13 +41,74 @@ module OrderingTests =
                                     ]
                             ]
                      ])
-                    |> snd
-                    |> List.map (fun (x, y) ->
-                        $"{x.ToString()} {y.ToString()}")
-                    |> String.concat ""
                     
-               testEqual
-                   "Test creating css with custom classname"
-                   normalSelectorMedia
+                testEqual
+                   "Ordering 1"
+                   orderingOne
                    "orderingOne { display: flex;flex-direction: column; }orderingOne > h2 { margin-bottom: 16px !important; }@media (min-width: 200px) { orderingOne { flex-direction: row;flex-grow: 1; }orderingOne > h2 { margin-bottom: 0px !important;margin-right: 16px !important; }orderingOne > h2:last-child { margin-right: 0px !important; } }"
+                   
+                let orderingTwo =
+                    createOrderTest "orderingTwo" [
+                        Color.orangeRed
+                        BackgroundColor.blue
+                        TextDecorationColor.yellow
+                        
+                        FirstOfType [
+                            BorderColor.gray
+                            BorderWidth.value (px 2)
+                            BorderStyle.dashed
+                        ]
+                    ]
+                    
+                testEqual
+                   "Ordering 2"
+                   orderingTwo
+                   "orderingTwo { color: orangered;background-color: blue;text-decoration-color: yellow; }orderingTwo:first-of-type { border-color: gray; border-width: 2px; border-style: dashed; }"
+                   
+                let orderingThree =
+                    createOrderTest "orderingThree" [
+                        Hover [
+                            FontWeight.bolder
+                        ]
+                        FontWeight.normal
+                        !> Html.P [
+                            FontWeight.bold
+                        ]
+                        Media.query
+                            [ Fss.Types.Media.MaxWidth (px 200) ]
+                            [ FontWeight.lighter ]
+                        !> Html.P [
+                            Color.red
+                        ]
+                    ]
+                   
+                testEqual
+                  "Ordering 3"
+                   orderingThree
+                   "orderingThree:hover { font-weight: bolder; }orderingThree { font-weight: normal; }orderingThree > p { font-weight: bold; }@media (max-width: 200px) { orderingThree { font-weight: lighter; } }orderingThree > p { color: red; }"
+                   
+                let orderingFour =
+                    createOrderTest "orderingFour" [
+                        !> Html.P [
+                            FontWeight.bold
+                        ]
+                        Media.query
+                            [ Fss.Types.Media.MaxWidth (px 200) ]
+                            [ FontWeight.lighter ]
+                        Hover [
+                            FontWeight.bolder
+                        ]
+                        FontWeight.normal
+                    ]
+                   
+                testEqual
+                  "Ordering 4"
+                   orderingFour
+                   "orderingFour > p { font-weight: bold; }@media (max-width: 200px) { orderingFour { font-weight: lighter; } }orderingFour:hover { font-weight: bolder; }orderingFour { font-weight: normal; }"
+
            ]
+           
+           // Combinator
+           // Media
+           // Pseudo
+           // Main
