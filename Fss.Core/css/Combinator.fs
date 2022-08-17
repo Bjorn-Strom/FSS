84a0973e53a36623dce5b1b246b161b94d6c2a4b
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-08-17 13:51:44 +0200

    Rename selector to combinator

--- Fss.Core/css/Combinator.fs
+++ Fss.Core/css/Combinator.fs
@@ -3,7 +3,7 @@ namespace Fss
 open Fss.Types
 
 [<AutoOpen>]
-module Selector =
+module Combinator =
     let (!+) (html: Html.Html) (rules: Rule list) = CombinatorClasses.Combinator.adjacentSibling html rules
     let (!~) (html: Html.Html) (rules: Rule list) = CombinatorClasses.Combinator.generalSibling html rules
     let (!>) (html: Html.Html) (rules: Rule list) = CombinatorClasses.Combinator.child html rules
