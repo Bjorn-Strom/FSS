dd59066d5944593681acc422411dd3eadc0bc0d6
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2023-02-18 22:49:57 +0100

    Update FET

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -5,13 +5,14 @@ open Fet
 module Tests =
 
     [<EntryPoint>]
-    let main(args) =
+    let main args =
         match Seq.tryHead args with
         | Some args ->
             if args = "bench" then
                 printfn "Generating some random CSS and timing it..."
                 let timingList = System.Collections.Generic.List()
-                for i in 0 .. 100 do
+
+                for i in 0..100 do
                     let randomRules = Generators.createRandomRules i
                     let start = System.DateTime.Now
                     Fss.Functions.createFss randomRules |> ignore
@@ -21,61 +22,60 @@ module Tests =
 
                 printfn "Performance test complete"
                 printfn "Number of rules, Milliseconds used"
-                Seq.iter (fun (n, ms) ->
-                    printfn $"{n}, {ms}"
-                    ) timingList
+                Seq.iter (fun (n, ms) -> printfn $"{n}, {ms}") timingList
+
+            0
         | None ->
-            runTests [
-                BorderTests.tests
-                ContentSizeTests.tests
-                MarginTests.tests
-                PaddingTests.tests
-                ColorTests.tests
-                BackgroundTests.tests
-                CursorTests.tests
-                GridTests.tests
-                DisplayTests.tests
-                FlexTests.tests
-                PerspectiveTests.tests
-                TextTests.tests
-                PositionTests.tests
-                ResizeTests.tests
-                TransitionTests.tests
-                VisibilityTests.tests
-                ListStyleTests.tests
-                ContentTests.tests
-                ColumnTests.tests
-                OutlineTests.tests
-                PointerEventsTests.tests
-                CaretTests.tests
-                ClipPathTests.tests
-                AspectRatioTests.tests
-                ClearTests.tests
-                AppearanceTests.tests
-                TypographyTests.tests
-                MixBlendModeTests.tests
-                FilterTests.tests
-                CustomTests.tests
-                BoxShadowTests.tests
-                AllTests.tests
-                ScrollTests.tests
-                TableTests.tests
-                WordTests.tests
-                WillChangeTests.tests
-                ImageTests.tests
-                MaskTests.tests
-                SvgTests.tests
-                CssGenerationTests.tests
-                MediaTests.tests
-                TransformTests.tests
-                PseudoTests.tests
-                FontTests.tests
-                OrderingTests.tests
-                CombinatorTests.tests
-                AnimationTests.tests
-                CounterTests.tests
-                FontFaceTests.tests
-                CompositeTests.tests
-                AttributeTests.tests
-            ]
-        0
+            runTests
+                [ BorderTests.tests
+                  ContentSizeTests.tests
+                  MarginTests.tests
+                  PaddingTests.tests
+                  ColorTests.tests
+                  BackgroundTests.tests
+                  CursorTests.tests
+                  GridTests.tests
+                  DisplayTests.tests
+                  FlexTests.tests
+                  PerspectiveTests.tests
+                  TextTests.tests
+                  PositionTests.tests
+                  ResizeTests.tests
+                  TransitionTests.tests
+                  VisibilityTests.tests
+                  ListStyleTests.tests
+                  ContentTests.tests
+                  ColumnTests.tests
+                  OutlineTests.tests
+                  PointerEventsTests.tests
+                  CaretTests.tests
+                  ClipPathTests.tests
+                  AspectRatioTests.tests
+                  ClearTests.tests
+                  AppearanceTests.tests
+                  TypographyTests.tests
+                  MixBlendModeTests.tests
+                  FilterTests.tests
+                  CustomTests.tests
+                  BoxShadowTests.tests
+                  AllTests.tests
+                  ScrollTests.tests
+                  TableTests.tests
+                  WordTests.tests
+                  WillChangeTests.tests
+                  ImageTests.tests
+                  MaskTests.tests
+                  SvgTests.tests
+                  CssGenerationTests.tests
+                  MediaTests.tests
+                  TransformTests.tests
+                  PseudoTests.tests
+                  FontTests.tests
+                  OrderingTests.tests
+                  CombinatorTests.tests
+                  AnimationTests.tests
+                  CounterTests.tests
+                  FontFaceTests.tests
+                  CompositeTests.tests
+                  AttributeTests.tests
+                ]
